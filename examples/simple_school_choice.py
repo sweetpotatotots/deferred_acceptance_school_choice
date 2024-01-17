7228ac00ce46e0b803a5990dd22026d6c39a19e0
sweetpotatotots <77068919+sweetpotatotots@users.noreply.github.com>
2024-01-16 18:15:28 -0800

    math ia fix

--- examples/simple_school_choice.py
+++ examples/simple_school_choice.py
@@ -7,15 +7,26 @@ def simple_school_choice() -> None:
     Here is a minimalistic example of deferred acceptance algorithm for school choice.
     """
     # Prepare the dataframes
-    students_list = ["a", "b", "c", "d"]
-    schools_list = ["A", "B", "C"]
+    students_list = ["a", "b", "c", "d", "e", "f", "g", "h"]
+    schools_list = ["BRW", "COL", "CRN", "DRT", "HAR", "PEN", "PTN", "YLE"]
     students_preferences = {
-        "a": [1, 2, 3],
-        "b": [2, 3, 1],
-        "c": [3, 2, 1],
-        "d": [2, 1, 3],
+        "a": [3, 1, 6, 7, 5, 8, 4, 2],
+        "b": [6, 5, 1, 7, 2, 4, 3, 8],
+        "c": [3, 2, 4, 6, 8, 7, 5, 1],
+        "d": [7, 5, 4, 6, 8, 2, 1, 3],
+        "e": [7, 4, 5, 3, 2, 6, 1, 8],
+        "f": [2, 5, 3, 8, 1, 7, 6, 4].
+        "g": [5, 1, 3, 2, 8, 7, 4, 6], 
+        "h": [1, 8, 2, 6, 7, 4, 3, 5]
     }
-    schools_preferences = {"A": [1, 2, 3, 4], "B": [1, 3, 2, 4], "C": [2, 3, 4, 1]}
+    schools_preferences = {"BRW": [2, 6, 4, 1, 7, 8, 5, 3], 
+                           "COL": [8, 7, 3, 6, 5, 2, 1, 4], 
+                           "CRN": [3, 8, 1, 4, 5, 7, 2, 6], 
+                           "DRT": [8, 2, 6, 7, 3, 4, 1, 5], 
+                           "HAR": [8, 4, 6, 2, 3, 5, 1, 7], 
+                           "PEN": [4, 1, 3, 8, 2, 6, 5, 7], 
+                           "PTN": [1, 8, 4, 7, 3, 6, 2, 5], 
+                           "YLE": [5, 3, 1, 7, 4, 6, 2, 8]}
 
     students_df, schools_df = create_dataframes(
         students_list=students_list,
